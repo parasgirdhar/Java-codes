da60a296da9f7433e8d3895c527d9e37edecda6d
parasgirdhar <parasy219@gmail.com>
2025-11-04 22:27:21 +0530

    Add files via upload

---
+++ Array_questions/sub_string_ary.java
@@ -0,0 +1,15 @@
+class  sub_string{
+public static void main(String[] args) {
+    
+int sum=0;
+int ary[]={2,7,5,9};
+
+for(int i=0;i<=ary.length-1;i++){
+     sum=sum+ary[i];
+    
+}
+System.out.print(sum);
+
+}
+
+}
