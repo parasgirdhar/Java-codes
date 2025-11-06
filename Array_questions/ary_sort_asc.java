fcf4207874c4c3abbc5cf4bd7e819fee39401aab
parasgirdhar <parasy219@gmail.com>
2025-11-06 23:00:07 +0530

    sort array in ascending order

--- Array_questions/ary_sort_asc.java
+++ Array_questions/ary_sort_asc.java
@@ -1,15 +1,17 @@
+// sort array in ascending order...
+
 import java.util.Arrays;
 
 class sort_array{
     public static void main(String[] args) {
       
         
-        int arr[]={2,7,2,8,4,9,6,4,7,1};
-
+  int arr[]={2,7,2,8,4,9,6,4,7,1};
+        
 Arrays.sort(arr);
-
+        
 System.out.println(Arrays.toString(arr));
-
 
     }
 }
+
