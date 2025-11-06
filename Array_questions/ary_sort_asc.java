9e97e1829437eb3826a3d0aa66f788e1625f211d
parasgirdhar <parasy219@gmail.com>
2025-11-06 22:58:17 +0530

    Add files via upload

---
+++ Array_questions/ary_sort_asc.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class sort_array{
+    public static void main(String[] args) {
+      
+        
+        int arr[]={2,7,2,8,4,9,6,4,7,1};
+
+Arrays.sort(arr);
+
+System.out.println(Arrays.toString(arr));
+
+
+    }
+}
---
+++ Array_questions/ary_sort_des.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+class des_array{
+    public static void main(String[] args) {
+        
+Integer arr[]={2,6,4,8,7,9,2,8,1,3};
+
+
+ Arrays.sort(arr, Collections.reverseOrder());
+
+System.out.println(Arrays.toString(arr));
+
+    }
+}
