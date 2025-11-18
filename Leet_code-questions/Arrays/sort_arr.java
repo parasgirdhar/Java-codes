5d93386f7f642c5ff6d010bf22e2ab76ec46734c
parasgirdhar <parasy219@gmail.com>
2025-11-18 22:22:16 +0530

    Add files via upload

---
+++ Leet_code-questions/Arrays/sort_arr.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class mainclass{
+    public static void main(String[] args) {
+        
+int arr[]={3,4,7,5,9,8,6,1,12,65,87,34,25};
+Arrays.sort(arr);
+System.out.println(Arrays.toString(arr));
+
+    }
+}
