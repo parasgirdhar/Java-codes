092710a968af6366c50e7b0bc2abf27889735cd7
parasgirdhar <parasy219@gmail.com>
2025-11-20 22:59:20 +0530

    Add files via upload

---
+++ Leet_code-questions/Arrays/sort_des_ary.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class descending {
+    public static void main(String[] args) {
+       
+Integer arr[]={6,4,8,7,2,9,1,3};
+
+Arrays.sort(arr,Collections.reverseOrder());
+
+System.out.print(Arrays.toString(arr)); 
+    }
+
+}
