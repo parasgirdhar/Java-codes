33f73f9d1d50969714a1a01971c514afc5b74c15
parasgirdhar <parasy219@gmail.com>
2026-01-10 00:09:40 +0530

    Add files via upload

---
+++ Array_questions/ary.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String []args){
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of array:");
+        int n=sc.nextInt();
+
+        int arr[]=new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("The result is:");
+        for(int i=0;i<n;i++){
+            System.out.print(" " + arr[i]);
+        }
+    }
+}
