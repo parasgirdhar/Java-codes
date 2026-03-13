9e7aac8f903165d5d75880cfad211ac01e817f15
parasgirdhar <parasy219@gmail.com>
2026-03-13 13:48:33 +0000

    fix very basic Errors in java

---
+++ FIX ERRORS/13-03-26-fix-error.java
@@ -0,0 +1,95 @@
+// problem 1....
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World")
+    }
+}
+
+
+// fixed with semicolon mistake in 14 no. line...
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }         
+}             
+
+
+// problem - 2........
+
+public class Test2 {
+    public static void main(String[] args) {
+        int number;
+        System.out.println(number);
+    }
+}
+
+
+// Fixed with variable initilized in 33 no. line.....
+
+public class Test2 {
+    public static void main(String[] args) {
+        int number=1;
+        System.out.println(number);
+    }
+}
+
+
+// problem - 3........
+
+
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.prinln("Java is fun!");
+    }
+}
+
+
+// Fixed By Correcting Method Name in 54 no. line.....
+
+
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println("Java is fun!");
+    }
+}
+
+
+// problem - 4........
+
+public class Test4 {
+    public static void main(String[] args) {
+        System.out.println("Fix me!");
+
+
+// Fixed By Applying missing Braces in the end.....
+
+        public class Test4 {
+    public static void main(String[] args) {
+        System.out.println("Fix me!");
+
+    }
+}
+
+
+
+// problem - 5........
+
+
+public class Test5 {
+    public static void main(String[] args) {
+        int x = "10";
+        System.out.println(x);
+    }
+}
+
+// Fixed by Removing double quotes on int x=10; beacuse it is a number and it is Int type not String
+
+public class Test5 {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x);
+    }
+}
+
