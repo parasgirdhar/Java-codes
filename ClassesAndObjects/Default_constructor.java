c0a29697d62c93f4f841ee6dd840f130c146c5a4
parasgirdhar <parasy219@gmail.com>
2026-03-23 18:43:22 +0530

    Understand about default constructor

---
+++ ClassesAndObjects/Default_constructor.java
@@ -0,0 +1,23 @@
+class pre{
+    
+    
+    int x;
+    int y;
+    
+    void fun1(){
+        System.out.println(x);
+        System.out.println(y);
+    }
+    
+}
+
+
+class Main{
+    public static void main(String []args){
+        
+        pre ref=new pre();
+        ref.fun1();
+        
+    }
+    
+}
