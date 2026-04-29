b59733f030421c4bb87fdd754eab801b54341b3e
parasgirdhar <parasy219@gmail.com>
2026-04-29 17:43:04 +0530

    Learn about Stack and Heap memory

---
+++ Java_Advance_0.2/Stack&Heap.java
@@ -0,0 +1,23 @@
+public class Stack&Heap {
+    public static void main(String []args){
+
+
+// ....................... Stack vs Heap in memory ....................................
+
+
+
+int arr= new int[4];
+
+// ehde vich jo arr hai ehy ek refrence variable hai ... ehy hamesha stack memory de andar banu gaa apne jine ve primitve datatype hai ehy apne stack memory de andar bndy hai ja store hundy hai 
+
+// hun jo new int [] hai ehy ek non- primitve datatype hai kyoki array non-primitive hai ty ehy sare de sare heap memory de andar store hundyy hai....
+
+
+// jehde refrence hundy hai ehy basically ek trha dy arrow hundy hai jo heap memory vich address nu point krdy hai....
+
+int x = 6;  // ehy primitive hai apa nu pta hai x directly hold the value of 6 ....
+
+int nums = new int [9];   // hun ehde andar nums refrence hai pur ehy diresctly array nu hold ne krda balki ohda address store krda hai ya array nu point krda hai......  kyoki ehy non-primitive hai 
+
+    }
+}
